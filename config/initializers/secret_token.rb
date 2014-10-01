c86fced98a55905b3e92815f7aa0de26216905ec
Manish Johari <manish.johari@kiwitech.com>
2014-10-01 11:41:02 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanyam::Application.config.secret_token = '8e4567fed6a0f1bcc36f4a9593fa6da64eb1fe39124da60dd0e856394e5e74962618ae647c8a35cde9e0bd11d162fac2b08a6906f340577697afc3f13d1cfd09'
